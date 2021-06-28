291517f27be7fe3b324dca6ed9b2899fb79e3759
Rene Dolores <rene.dolores@mercadolivre.com>
2021-06-28 13:10:33 -0300

    removido teste inutil

--- src/test/kotlin/com/example/reactive/ApiWebClientTest.kt
+++ src/test/kotlin/com/example/reactive/ApiWebClientTest.kt
@@ -1,30 +1,14 @@
 package com.example.reactive
 
 
-import org.awaitility.Awaitility.await
 import org.junit.jupiter.api.Test
-import org.slf4j.LoggerFactory
 import org.springframework.boot.test.context.SpringBootTest
 import reactor.test.StepVerifier
-import java.util.concurrent.atomic.AtomicBoolean
 
 
 @SpringBootTest
-internal class ApiWebClientTest() {
+internal class ApiWebClientTest {
     val apiWebClient: ApiWebClient = ApiWebClient()
-    private val log = LoggerFactory.getLogger(javaClass)
-    @Test
-    fun testGetActivity(){
-        var taskDone : AtomicBoolean = AtomicBoolean(false)
-        apiWebClient.getActivity().doOnSuccess {
-            log.info(it.toString())
-        }.doOnError {
-            log.info("erro na chamada")
-        }.doFinally { taskDone.set(true) }
-        .subscribe()
-
-        await().untilTrue(taskDone)
-    }
 
     @Test
     fun testStepVerifier() {
