0a23734bb3975db2bc508150a1cc66af7c50110e
Rene Dolores <rene.dolores@mercadolivre.com>
2021-06-28 13:19:04 -0300

    refactor

--- src/main/kotlin/com/example/reactive/ApiHandlers.kt
+++ src/main/kotlin/com/example/reactive/ApiHandlers.kt
@@ -1,10 +1,15 @@
 package com.example.reactive
 
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
-import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.BodyInserters
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.ServerResponse.ok
 import reactor.core.publisher.Mono
 
 @Component
 class ApiHandlers(val webClient: ApiWebClient) {
-    fun activityHandler(request: ServerRequest) : Mono<ActivityModel> = webClient.getActivity().log()
+    fun activityHandler() : Mono<ServerResponse> = ok()
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(BodyInserters.fromPublisher(webClient.getActivity().log(), ActivityModel::class.java))
 }
--- src/main/kotlin/com/example/reactive/ApiRouter.kt
+++ src/main/kotlin/com/example/reactive/ApiRouter.kt
@@ -2,8 +2,6 @@ package com.example.reactive
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.MediaType
-import org.springframework.web.reactive.function.BodyInserters.fromPublisher
 import org.springframework.web.reactive.function.server.router
 
 
@@ -11,8 +9,6 @@ import org.springframework.web.reactive.function.server.router
 class ApiRouter {
     @Bean
     fun mainRoute(handler: ApiHandlers) = router {
-            GET("/") { ok()
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(fromPublisher(handler.activityHandler(it), ActivityModel::class.java)) }
+            GET("/") { handler.activityHandler() }
         }
 }
